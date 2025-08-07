f8d7f89d71a90c0b61732629db63c66015e840b0
DybyTech <dybytech00@gmail.com>
2025-08-07 06:43:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    CHATBOT: getConfig("CHATBOT") || "on",
-    //chatbot on/off
-  
+
     SESSION_ID: process.env.SESSION_ID || "MEGALODON-MD~",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
